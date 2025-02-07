b4bab87731dd55592593e926d472ed46dcf94ef7
Turner <twklaphe@ncsu.edu>
2025-02-07 15:16:52 -0500

    pushing into  repo

---
+++ mips5_current.asm
@@ -0,0 +1,489 @@
+.data 0x10010000
+board: .word 	0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+		
+		
+grid: .word 	0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00FFFFFF, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00FFFFFF, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+
+		0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+		
+
+X: .word	0x00FF0000, 0x00000000, 0x00000000, 0x00FF0000,
+
+		0x00000000, 0x00FF0000, 0x00FF0000, 0x00000000,
+		
+		0x00000000, 0x00FF0000, 0x00FF0000, 0x00000000,
+		
+		0x00FF0000, 0x00000000, 0x00000000, 0x00FF0000,
+
+
+o: .word	0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF,
+
+		0x000000FF, 0x00000000, 0x00000000, 0x000000FF,
+		
+		0x000000FF, 0x00000000, 0x00000000, 0x000000FF,
+		
+		0x000000FF, 0x000000FF, 0x000000FF, 0x000000FF,
+
+q: .half 68
+w: .half 88
+e: .half 108
+a: .half 388
+s: .half 408
+d: .half 428
+z: .half 708
+x: .half 728
+c: .half 748
+
+input_request: .asciiz "Enter player input\n"
+input_complete: .asciiz "Input recieved\n\n"
+input_illegal: .asciiz "That play isn't legal, "
+enemy_out: .asciiz "enemy "
+x_win_text: .asciiz "Player X wins the game!!!\n\n"
+o_win_text: .asciiz "Player O wins the game!!!\n\n"
+draw_text: .asciiz "Draw...\n\n"
+replay_text: .asciiz "Would you like to play again???  (y/n)\n\n"
+turn_dialogue: .asciiz "Would you like to go first? y/n? \n\n"
+play_bot: .asciiz "Would you like to play a bot? y/n? \n\n"
+
+.text  0x00400000
+main:
+	li $s4, 0
+	jal setup
+	jal playertype
+game:
+	jal input
+	jal write_x
+	li $s3, 0x00FF0000
+	add $s4, $s4, 1
+	jal win_check
+player2:
+	jal playertwo
+	jal write_o
+	li $s3, 0x000000FF
+	add $s4, $s4, 1
+	jal win_check
+	j game
+	
+end:
+	li $v0, 10
+	syscall
+
+
+setup:
+	li $t1, 0
+	li $t2, 4
+	li $t3, 1024
+	setup_loop:
+		lw $t4, grid($t1)
+		sw $t4, board($t1)
+		addu $t1, $t1, $t2
+		bne $t1, $t3, setup_loop 
+	jr $ra
+	
+refresh:
+	li $t1, 0
+	li $t2, 4
+	li $t3, 1024
+	refresh_loop:
+		lw $t4, board($t1)
+		sw $t4, board($t1)
+		addu $t1, $t1, $t2
+		bne $t1, $t3, refresh_loop 
+	jr $ra
+
+write_x:
+	la $t0, board
+	move $t1, $a1
+	li $t2, 0 				#i				
+	li $t3, 0				#j
+	i_loop:
+		j_loop:
+			sll $t4, $t2, 4			#computing address in array
+			add $t4, $t4, $t3
+			lw  $a0, X($t4)
+		
+			sll $t4, $t2, 6
+			add $t4, $t4, $t3
+			add $t4, $t4, $t1
+			sw $a0, board($t4)
+		
+			addi $t3, $t3, 4		#increment j
+			ble $t3, 12, j_loop
+		
+		li $t3, 0				#reset j
+		addi $t2, $t2, 1			#increment i	
+		ble $t2, 3, i_loop
+	jr $ra
+	
+write_o:
+	la $t0, board
+	move $t1, $a1
+	li $t2, 0 				#i				
+	li $t3, 0				#j
+	i_loop2:
+		j_loop2:
+			sll $t4, $t2, 4			#computing address in array
+			add $t4, $t4, $t3
+			lw  $a0, o($t4)
+		
+			sll $t4, $t2, 6
+			add $t4, $t4, $t3
+			add $t4, $t4, $t1
+			sw $a0, board($t4)
+		
+			addi $t3, $t3, 4		#increment j
+			ble $t3, 12, j_loop2
+		
+		li $t3, 0				#reset j
+		addi $t2, $t2, 1			#increment i	
+		ble $t2, 3, i_loop2
+	jr $ra
+	
+input:
+	la $a0, input_request
+	li $v0 4
+	syscall
+	
+	wait:
+		lw $t0, 0xffff0008
+		beqz $t0, wait
+	
+	lw $a1, 0xFFFF0004
+	sw $0, 0xFFFF0004
+	
+	beq $a1, 'q', legal_q
+	beq $a1, 'w', legal_w
+	beq $a1, 'e', legal_e
+	beq $a1, 'a', legal_a
+	beq $a1, 's', legal_s
+	beq $a1, 'd', legal_d
+	beq $a1, 'z', legal_z
+	beq $a1, 'x', legal_x
+	beq $a1, 'c', legal_c
+	j wait
+	
+	legal_q:
+		lh $t1, q
+		lw $t0, board($t1)
+		beqz $t0, input_q
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_w:
+		lh $t1, w
+		lw $t0, board($t1)
+		beqz $t0, input_w
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_e:
+		lh $t1, e
+		lw $t0, board($t1)
+		beqz $t0, input_e
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_a:
+		lh $t1, a
+		lw $t0, board($t1)
+		beqz $t0, input_a
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_s:
+		lh $t1, s
+		lw $t0, board($t1)
+		beqz $t0, input_s
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_d:
+		lh $t1, d
+		lw $t0, board($t1)
+		beqz $t0, input_d
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_z:
+		lh $t1, z
+		lw $t0, board($t1)
+		beqz $t0, input_z
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_x:
+		lh $t1, x
+		lw $t0, board($t1)
+		beqz $t0, input_x
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+	legal_c:
+		lh $t1, c
+		lw $t0, board($t1)
+		beqz $t0, input_c
+		la $a0, input_illegal
+		li $v0 4
+		syscall
+		add $ra, $ra, -4
+		jr $ra
+		
+		
+		input_q:
+		lh $a1, q
+		j input_done
+		input_w:
+		lh $a1, w
+		j input_done
+		input_e:
+		lh $a1, e
+		j input_done
+		input_a:
+		lh $a1, a
+		j input_done
+		input_s:
+		lh $a1, s
+		j input_done
+		input_d:
+		lh $a1, d
+		j input_done
+		input_z:
+		lh $a1, z
+		j input_done
+		input_x:
+		lh $a1, x
+		j input_done
+		input_c:
+		lh $a1, c
+input_done:
+	la $a0, input_complete
+	li $v0 4
+	syscall
+	
+	jr $ra
+
+enemy:
+	la $a0, enemy_out
+	li $v0, 4
+	syscall
+	
+	li $a1, 8	#loads rand int to $a0
+	li $v0, 42
+	syscall
+	
+	beq $a0, 0, legal_q #branches to input coresponding to enemy rand int
+	beq $a0, 1, legal_w
+	beq $a0, 2, legal_e
+	beq $a0, 3, legal_a
+	beq $a0, 4, legal_s
+	beq $a0, 5, legal_d
+	beq $a0, 6, legal_z
+	beq $a0, 7, legal_x
+	beq $a0, 8, legal_c
+	
+win_check:
+	lh $t0, q		#converts each square to true/false 1/0
+	lw $t0, board($t0)
+	seq $t0, $t0, $s3
+	lh $t1, w
+	lw $t1, board($t1)
+	seq $t1, $t1, $s3
+	lh $t2, e
+	lw $t2, board($t2)
+	seq $t2, $t2, $s3
+	lh $t3, a
+	lw $t3, board($t3)
+	seq $t3, $t3, $s3
+	lh $t4, s
+	lw $t4, board($t4)
+	seq $t4, $t4, $s3
+	lh $t5, d
+	lw $t5, board($t5)
+	seq $t5, $t5, $s3
+	lh $t6, z
+	lw $t6, board($t6)
+	seq $t6, $t6, $s3
+	lh $t7, x
+	lw $t7, board($t7)
+	seq $t7, $t7, $s3
+	lh $t8, c
+	lw $t8, board($t8)
+	seq $t8, $t8, $s3
+
+	add $a0, $t0, $t1 #win condition qwe
+	add $a0, $a0, $t2
+	beq $a0, 3, win
+	
+	add $a0, $t3, $t4 #win condition asd
+	add $a0, $a0, $t5
+	beq $a0, 3, win
+	
+	add $a0, $t6, $t7 #win condition zxc
+	add $a0, $a0, $t8
+	beq $a0, 3, win
+	
+	add $a0, $t0, $t3 #win condition qaz
+	add $a0, $a0, $t6
+	beq $a0, 3, win
+	
+	add $a0, $t1, $t4 #win condition wsx
+	add $a0, $a0, $t7
+	beq $a0, 3, win
+	
+	add $a0, $t2, $t5 #win condition edc
+	add $a0, $a0, $t8
+	beq $a0, 3, win
+	
+	add $a0, $t0, $t4 #win condition qsc
+	add $a0, $a0, $t8
+	beq $a0, 3, win
+	
+	add $a0, $t2, $t4 #win condition esz
+	add $a0, $a0, $t6
+	beq $a0, 3, win
+	
+	beq $s4, 9, draw
+	
+	jr $ra	#exit no win
+	
+win:
+	beq $s3, 0x000000FF, o_wins
+	
+	la $a0, x_win_text
+	li $v0, 4
+	syscall
+	
+	j replay
+	
+	o_wins:	
+		la $a0, o_win_text
+		li $v0, 4
+		syscall
+	
+	j replay
+	
+draw:
+	la $a0, draw_text
+	li $v0, 4
+	syscall
+	
+	j replay
+	
+replay:
+	la $a0, replay_text
+	li $v0, 4
+	syscall
+	
+	waitR:
+		lw $t0, 0xffff0008
+		beqz $t0, waitR
+	
+	lw $a1, 0xFFFF0004
+	sw $0, 0xFFFF0004
+	
+	beq $a1, 'y', main
+	beq $a1, 'n', end
+	
+	j waitR
+
+playertype:
+	la $a0, play_bot
+	li $v0, 4
+	syscall
+	waitB:
+		lw $t0, 0xffff0008
+		beqz $t0, waitB
+	
+	lw $a2, 0xFFFF0004
+	sw $0, 0xFFFF0004
+	
+	beq $a2, 'y', bot
+	beq $a2, 'n', human
+	j waitB
+	bot:
+		li $s0, 0
+		jr $ra
+	human:
+		li $s0, 1
+		jr $ra
+	
+	j waitB
+
+
+playertwo:
+	beqz $s0 enemy
+	bnez $s0 input
